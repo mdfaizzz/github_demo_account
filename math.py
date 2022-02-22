dd9b5da8abee8814b489441f549ba41a1d6d74cc
Md Faizan <79753913+mdfaizzz@users.noreply.github.com>
2022-02-22 18:43:14 +0530

    Initial commit of math.py file

---
+++ math.py
@@ -0,0 +1,16 @@
+def add(x,y):
+ return x+y
+def subtract(x,y):
+ return x-y             
+
+def multiply(x,y):              
+ return x*y
+
+def divide(x,y):             
+ if y==0 :
+       return DIVIDE_BY_ZERO_ERROR
+ else :
+       return x/y 
+
+def square(x):
+   return x*x
